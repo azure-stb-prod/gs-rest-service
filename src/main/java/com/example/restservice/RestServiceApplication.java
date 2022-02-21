79f35dab10f025f17ea44d71db2ec1930a3bb428
Puneet Goyal <PuneetKumarGoyal@gmail.com>
2022-02-21 21:25:17 +0530

    trying to expose env variable via application

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,13 +1,20 @@
 package com.example.restservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestServiceApplication {
+public class RestServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("RestApplicationService has started");
+        System.out.println("Environment_prod_secret: " + System.getenv("ENVIRONMENT_PROD_SECRET"));
     }
 
 }
